9aa4cc25b2e23530032ca94f476fad611de6a522
Ira Lialikova <lialikova.ira@gmail.com>
2015-09-14 00:06:26 +0300

    Create property in the persistence pom.xml to store hibernate version, change author in package-info file

--- common/src/main/java/com/softserveinc/ita/jresume/common/entity/package-info.java
+++ common/src/main/java/com/softserveinc/ita/jresume/common/entity/package-info.java
@@ -2,7 +2,7 @@
  * Provides the classes necessary to store an information about all entities
  * which form the basis of entire database.
  * 
- * @author Ira
+ * @author 159-Java.2
  */
 package com.softserveinc.ita.jresume.common.entity;
 
