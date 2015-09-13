e3c54d9120034ddff577dea101a58b37b1a38bd4
Golovii Sergii <golovii.sergii@gmail.com>
2015-09-13 20:25:57 +0300

    Fix project build. Remove dev/readme.txt. Remove entity folder.

--- common/src/main/java/com/softserveinc/ita/jresume/common/entity/package-info.java
+++ common/src/main/java/com/softserveinc/ita/jresume/common/entity/package-info.java
@@ -1,8 +1,8 @@
 /**
- * Provides the classes necessary to store an  
- * information about all entities which form 
- * the basis of entire database
+ * Provides the classes necessary to store an information about all entities
+ * which form the basis of entire database.
  * 
  * @author Ira
  */
 package com.softserveinc.ita.jresume.common.entity;
+
--- dev/readme.txt
+++
@@ -1,1 +0,0 @@
-Use this folder to save useful files.
