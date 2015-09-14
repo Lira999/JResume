62664d6f629ebdca97d85f6a0c761f9cfb282337
Ira Lialikova <lialikova.ira@gmail.com>
2015-09-14 10:59:13 +0300

    put back version in persistence and common

--- common/src/main/java/com/softserveinc/ita/jresume/common/entity/package-info.java
+++ common/src/main/java/com/softserveinc/ita/jresume/common/entity/package-info.java
@@ -2,7 +2,7 @@
  * Provides the classes necessary to store an information about all entities
  * which form the basis of entire database.
  * 
- * @author 159-Java.2
+ * @author Ira Lialikova
  */
 package com.softserveinc.ita.jresume.common.entity;
 
