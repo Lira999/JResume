896a9d3b556b127222e2b0c0db7962eea55e8531
Ira Lialikova <lialikova.ira@gmail.com>
2015-09-14 14:25:50 +0300

    Change inheritance mapping

--- common/src/main/java/com/softserveinc/ita/jresume/common/entity/Base.java
+++ common/src/main/java/com/softserveinc/ita/jresume/common/entity/Base.java
@@ -4,23 +4,21 @@
 package com.softserveinc.ita.jresume.common.entity;
 
 import java.util.Date;
-import javax.persistence.Entity;
+
 import javax.persistence.Id;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
 /**
  * Describe Base entity class.
  */
-@Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@MappedSuperclass
 public class Base {
     /** Column for Id. */
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     /** Column for creatBy. */
