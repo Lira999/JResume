5fe217d2c8280c6dee6e4b3a415436c6b43d0922
chyvaak <chyvaak90@gmail.com>
2015-09-16 19:08:09 +0300

    Add Javadocs

--- persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/GenericDAO.java
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/GenericDAO.java
@@ -1,41 +1,55 @@
 package com.softserveinc.ita.jresume.persistence.dao;
 
+import java.util.List;
+
 /**
  *
  * @param <Model>
  */
 public interface GenericDAO<Model> {
+    
     /**
      * 
      * @param entity
-     *            This describes my type parameter.
+     *            Create and save new object in DB.
+     * @return a unique element or null if none exists.
      */
-    void create(Model entity);
+    
+    Model create(Model entity);
     
     /**
      * 
      * @param id
-     *            This describes my type parameter.
-     * 
-     * @return Model
+     *           Gets a unique element with an id, assumes column name is id.
+     *            
+     * @return  a unique element or null if none exists
      */
+    
     Model findById(Long id);
     
     /**
      * 
      * @param entity
      *            choose Entity class to create a table.
-     * 
-     * @return Model
+     *            
+     * @return a unique element or null if none exists.
      */
+    
     Model update(Model entity);
     
     /**
      * 
      * @param id
-     *            This describes my type parameter.
-     * 
-     * @return Model
+     *            Remove the entity with the specified type and id from the data
+     *            store.
      */
-    Model delete(Long id);
+    
+    void delete(Long id);
+    
+    /**
+     * This loads all of the elements from the given table from the database.
+     * @return a list of all the elements in a table specified by the model.
+     */
+    
+    List<Model> findAll();
 }
--- persistence/src/main/java/com/softserveinc/ita/jresume/persistence/jpa/JPAGenericDAO.java
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/jpa/JPAGenericDAO.java
@@ -1,65 +1,90 @@
 package com.softserveinc.ita.jresume.persistence.jpa;
 
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import com.softserveinc.ita.jresume.persistence.dao.GenericDAO;
 
 /**
- * 
+ * Implementation of Generic DAO.
  *
- * @param <Model>
+ * @param <Model> 
+ *            The type of the domain object for which this instance is to be
+ *            used.
  */
-public abstract class JPAGenericDAO<Model> implements
-        GenericDAO<Model> {
+public abstract class JPAGenericDAO<Model> implements GenericDAO<Model> {
+    
     /**
-     * Javadoc.
+     * Entity manager is used to create / update / remove / find persistent
+     * entity instances, and to query over entities.
      */
     @PersistenceContext
     private EntityManager entityManager;
     /**
-     * Javadoc.
+     * Represents concrete Entity to work with.
      */
     private Class<Model> entityType;
+    
     /**
-     * Javadoc.
+     * Single public constructor. Turn back name of entity to work with.
      */
     @SuppressWarnings("unchecked")
     public JPAGenericDAO() {
-        this.entityType = (Class<Model>) ((ParameterizedType) getClass()
-                .getGenericSuperclass()).getActualTypeArguments()[0];
+        ParameterizedType genericSuperClass = (ParameterizedType) getClass()
+                .getGenericSuperclass();
+        this.entityType = (Class<Model>) genericSuperClass
+                .getActualTypeArguments()[0];
     }
-    /**
-     * Javadoc.
-     */
+    
     @Override
-    public final void create(final Model entity) {
+    public final Model create(final Model entity) {
         entityManager.persist(entity);
+        return entity;
     }
-    /**
-     * Javadoc.
-     */
+    
     @Override
     public final Model update(final Model entity) {
         return entityManager.merge(entity);
     }
-    /**
-     * Javadoc.
-     */
+    
     @Override
     public final Model findById(final Long id) {
         return entityManager.find(entityType, id);
     }
-    /**
-     * Javadoc.
-     */
+    
     @Override
-    public final Model delete(final Long id) {
+    public final void delete(final Long id) {
         Model entity = findById(id);
         entityManager.remove(entity);
-        return entity;
     }
     
+    @SuppressWarnings("unchecked")
+    @Override
+    public final List<Model> findAll() {
+        Query query = entityManager.createQuery(
+                "SELECT entity FROM " + entityType.getName() + " entity");
+        return query.getResultList();
+    }
+    
+    /**
+     * 
+     * @param newEntityManager
+     *            Set entityManager
+     */
+    
+    public final void setEntityManager(final EntityManager newEntityManager) {
+        this.entityManager = newEntityManager;
+    }
+    
+    /**
+     * 
+     * @return entityManager
+     */
+    public final EntityManager getEntityManager() {
+        return entityManager;
+    }
 }
