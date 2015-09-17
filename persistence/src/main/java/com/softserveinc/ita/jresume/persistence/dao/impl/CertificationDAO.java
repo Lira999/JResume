d93f403a57829ed133288e024ca8600e418eaf4b
Andriy Luchko <andruluchko@ukr.net>
2015-09-17 17:40:11 +0300

    add certification dao and jpa

---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/CertificationDAO.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl;
+
+import com.softserveinc.ita.jresume.common.entity.Certification;
+import com.softserveinc.ita.jresume.persistence.dao.GenericDAO;
+
+/**
+ * This interface describes CertificationDAO behavior.
+ * 
+ * @author Andriy Luchko
+ */
+public interface CertificationDAO extends GenericDAO<Certification, Long> {
+   
+}
---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPACertificationDAO.java
@@ -0,0 +1,14 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl.jpa;
+
+import com.softserveinc.ita.jresume.common.entity.Certification;
+import com.softserveinc.ita.jresume.persistence.dao.impl.CertificationDAO;
+
+/**
+ * Implementation of Certification DAO.
+ * 
+ * @author Andriy Luchko
+ */
+public class JPACertificationDAO extends JPAGenericDAO<Certification, Long>
+        implements CertificationDAO {
+        
+}
