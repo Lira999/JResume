eb5d26d6fb16dd15573d957f600df2fe954e3a89
Bohdan Khudoba <bohdanthebest@gmail.com>
2015-09-17 16:43:30 +0300

    Added UserInformationDAO

---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/UserInformationDAO.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl;
+
+import com.softserveinc.ita.jresume.common.entity.User;
+import com.softserveinc.ita.jresume.persistence.dao.GenericDAO;
+
+/**
+ * 
+ * @author Bohdan Khudoba
+ *         
+ */
+public interface UserInformationDAO extends GenericDAO<User, Long> {
+
+}
---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAUserInformationDAO.java
@@ -0,0 +1,14 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl.jpa;
+
+import com.softserveinc.ita.jresume.common.entity.User;
+import com.softserveinc.ita.jresume.persistence.dao.impl.UserInformationDAO;
+
+/**
+ * 
+ * @author Bohdan Khudoba
+ *         
+ */
+public class JPAUserInformationDAO extends JPAGenericDAO<User, Long>
+        implements UserInformationDAO {
+        
+}
