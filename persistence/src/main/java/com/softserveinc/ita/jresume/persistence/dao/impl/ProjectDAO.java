a75ef42fb077788357f6fe46f925b1742bb870cf
Andriy Luchko <andruluchko@ukr.net>
2015-09-17 17:41:44 +0300

    add project dao and jpa

---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/ProjectDAO.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl;
+
+import com.softserveinc.ita.jresume.common.entity.Project;
+import com.softserveinc.ita.jresume.persistence.dao.GenericDAO;
+
+/**
+ * This interface describes ProjectDAO behavior.
+ * 
+ * @author Andriy Luchko
+ */
+public interface ProjectDAO extends GenericDAO<Project, Long> {
+
+}
---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAProjectDAO.java
@@ -0,0 +1,14 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl.jpa;
+
+import com.softserveinc.ita.jresume.common.entity.Project;
+import com.softserveinc.ita.jresume.persistence.dao.impl.ProjectDAO;
+
+/**
+ * Implementation of Project DAO.
+ * 
+ * @author Andriy Luchko
+ */
+public class JPAProjectDAO extends JPAGenericDAO<Project, Long>
+        implements ProjectDAO {
+        
+}
