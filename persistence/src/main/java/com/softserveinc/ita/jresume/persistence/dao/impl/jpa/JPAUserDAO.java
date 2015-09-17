34cd4e5f6ba8cf633fd0e178866ef0e67e1af351
Andriy Zykhor <andriyzykhor@gmail.com>
2015-09-17 18:49:31 +0300

    Add implementation of UserDetailsService.

--- persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAUserDAO.java
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAUserDAO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.Query;
 
+import org.springframework.stereotype.Repository;
+
 import com.softserveinc.ita.jresume.common.entity.User;
 import com.softserveinc.ita.jresume.persistence.dao.impl.UserDAO;
 
@@ -13,6 +15,7 @@ import com.softserveinc.ita.jresume.persistence.dao.impl.UserDAO;
  * @author Andriy
  *         
  */
+@Repository
 public class JPAUserDAO extends JPAGenericDAO<User, Long>
         implements UserDAO {
         
