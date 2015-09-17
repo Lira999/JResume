bf57f92707978fa1775f19a6a4ce9550be5042b2
chyvaak <chyvaak90@gmail.com>
2015-09-17 21:58:18 +0300

    Add UserResumeDAO and JPAUserResumeDAO

---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/UserResumeDAO.java
@@ -0,0 +1,11 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl;
+
+import com.softserveinc.ita.jresume.common.entity.UserResume;
+import com.softserveinc.ita.jresume.persistence.dao.GenericDAO;
+
+/**
+ * Interface for UserResume DAO.
+ */
+public interface UserResumeDAO extends GenericDAO<UserResume, Long> {
+    
+}
