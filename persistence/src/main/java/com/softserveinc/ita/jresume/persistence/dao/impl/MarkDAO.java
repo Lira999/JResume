bb9765fcf715ee4eacaf0ca24474d2583d3432f7
Golovii Sergii <golovii.sergii@gmail.com>
2015-09-17 18:57:20 +0300

    Add MarkDAO and JPAMarkDAO.

---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/MarkDAO.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl;
+
+import com.softserveinc.ita.jresume.common.entity.Mark;
+import com.softserveinc.ita.jresume.persistence.dao.GenericDAO;
+
+/**
+ * Interface describes specific methods, used for working with Mark entity.
+ * 
+ * @author Golovii Sergii
+ */
+public interface MarkDAO extends GenericDAO<Mark, Long> {
+    
+}
---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAMarkDAO.java
@@ -0,0 +1,11 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl.jpa;
+
+import com.softserveinc.ita.jresume.common.entity.Mark;
+import com.softserveinc.ita.jresume.persistence.dao.impl.MarkDAO;
+/**
+ * JPA implementation for MarkDAO.
+ * @author Golovii Sergii
+ */
+public class JPAMarkDAO extends JPAGenericDAO<Mark, Long> implements MarkDAO {
+
+}
