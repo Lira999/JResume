6b8903758adfc09b1a589b0bf5d88160a6a123ad
Andriy Luchko <andruluchko@ukr.net>
2015-09-17 17:41:10 +0300

    add education dao and jpa

---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/EducationDAO.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl;
+
+import com.softserveinc.ita.jresume.common.entity.Education;
+import com.softserveinc.ita.jresume.persistence.dao.GenericDAO;
+
+/**
+ * This interface describes EducationDAO behavior.
+ * 
+ * @author Andriy Luchko
+ */
+public interface EducationDAO extends GenericDAO<Education, Long> {
+
+}
---
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAEducationDAO.java
@@ -0,0 +1,14 @@
+package com.softserveinc.ita.jresume.persistence.dao.impl.jpa;
+
+import com.softserveinc.ita.jresume.common.entity.Education;
+import com.softserveinc.ita.jresume.persistence.dao.impl.EducationDAO;
+
+/**
+ * Implementation of Education DAO.
+ * 
+ * @author Andriy Luchko
+ */
+public class JPAEducationDAO extends JPAGenericDAO<Education, Long>
+        implements EducationDAO {
+        
+}
