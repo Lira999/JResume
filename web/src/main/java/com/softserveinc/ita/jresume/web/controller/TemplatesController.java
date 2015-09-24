4c61f49396d3ec98311ed5af959f4dff99be2205
Ira Lialikova <lialikova.ira@gmail.com>
2015-09-24 12:21:28 +0300

    change TemplatesController

--- web/src/main/java/com/softserveinc/ita/jresume/web/controller/TemplatesController.java
+++ web/src/main/java/com/softserveinc/ita/jresume/web/controller/TemplatesController.java
@@ -3,7 +3,6 @@ package com.softserveinc.ita.jresume.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -17,17 +16,11 @@ public class TemplatesController {
     /**
      * Templates page mapping.
      * 
-     * @param error
-     *            Error message to display
      * @return model "templates" view
      */
     @RequestMapping(value = "/templates", method = RequestMethod.GET)
-    public final ModelAndView templates(@RequestParam(value = "error",
-            required = false) final String error) {
+    public final ModelAndView templates() {
         ModelAndView modelAndView = new ModelAndView();
-        if (error != null) {
-            modelAndView.addObject("error", "Incorect sign in data!");
-        }
         modelAndView.setViewName("templates");
         return modelAndView;
     }
