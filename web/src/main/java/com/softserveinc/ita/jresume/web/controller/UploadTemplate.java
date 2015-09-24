3e5759c97da26ebb56eb3e660ba049383c246085
chyvaak <chyvaak90@gmail.com>
2015-09-24 12:25:10 +0300

    fix controllers

--- web/src/main/java/com/softserveinc/ita/jresume/web/controller/UploadTemplate.java
+++ web/src/main/java/com/softserveinc/ita/jresume/web/controller/UploadTemplate.java
@@ -3,7 +3,6 @@ package com.softserveinc.ita.jresume.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -15,20 +14,15 @@ public class UploadTemplate {
     
     /**
      * Templates page mapping.
-     * 
-     * @param error
-     *            Error message to display
+     
      * @return model "upload template" view
      */
     @RequestMapping(value = "/uploadtemplate", method = RequestMethod.GET)
-    public final ModelAndView viewtempl(@RequestParam(value = "error",
-            required = false) final String error) {
+    public final ModelAndView viewtempl() {
         ModelAndView modelAndView = new ModelAndView();
-        if (error != null) {
-            modelAndView.addObject("error", "Incorect sign in data!");
-        }
+        
         modelAndView.setViewName("uploadtemplate");
         return modelAndView;
-    }
     
+    }
 }
--- web/src/main/java/com/softserveinc/ita/jresume/web/controller/ViewtemplController.java
+++ web/src/main/java/com/softserveinc/ita/jresume/web/controller/ViewtemplController.java
@@ -3,7 +3,6 @@ package com.softserveinc.ita.jresume.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -15,18 +14,13 @@ public class ViewtemplController {
     
     /**
      * Templates page mapping.
-     * 
-     * @param error
+     
      *            Error message to display
      * @return model "viewtempl" view
      */
     @RequestMapping(value = "/viewtempl", method = RequestMethod.GET)
-    public final ModelAndView viewtempl(@RequestParam(value = "error",
-            required = false) final String error) {
+    public final ModelAndView viewtempl() {
         ModelAndView modelAndView = new ModelAndView();
-        if (error != null) {
-            modelAndView.addObject("error", "Incorect sign in data!");
-        }
         modelAndView.setViewName("viewtempl");
         return modelAndView;
     }
