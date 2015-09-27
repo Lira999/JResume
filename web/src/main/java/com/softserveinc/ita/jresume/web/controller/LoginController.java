ae296c116136619c2f887d6cf79d3e5454cc6b16
Golovii Sergii <golovii.sergii@gmail.com>
2015-09-27 22:19:14 +0300

    Add error message in case of some internal problems (e.g. db connection)

--- web/src/main/java/com/softserveinc/ita/jresume/web/controller/LoginController.java
+++ web/src/main/java/com/softserveinc/ita/jresume/web/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.softserveinc.ita.jresume.web.controller;
 
+import org.springframework.security.authentication.
+
+InternalAuthenticationServiceException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -19,14 +23,32 @@ public class LoginController {
      * 
      * @param error
      *            Error message to display
-     * @return model "login" view
+     * @param webRequest
+     *            current web request
+     * @return modelAndView with "login" view and error message (if error
+     *         present)
      */
+    @SuppressWarnings("static-access")
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public final ModelAndView login(@RequestParam(value = "error",
-            required = false) final String error) {
+    public final ModelAndView login(final WebRequest webRequest,
+            @RequestParam(value = "error",
+                    required = false) final String error) {
         ModelAndView modelAndView = new ModelAndView();
+        Object lastException =
+                webRequest.getAttribute("SPRING_SECURITY_LAST_EXCEPTION",
+                        webRequest.SCOPE_SESSION);
         if (error != null) {
-            modelAndView.addObject("error", "Incorect sign in data!");
+            if (lastException
+            
+            instanceof InternalAuthenticationServiceException) {
+                modelAndView.addObject("error",
+                        "JResume encountered some internal problems. "
+                                + "We working to resolve the issue as "
+                                + "soon as possible."
+                                + " Please try again later.");
+            } else {
+                modelAndView.addObject("error", "Incorect sign in data!");
+            }
         }
         modelAndView.setViewName("login");
         return modelAndView;
