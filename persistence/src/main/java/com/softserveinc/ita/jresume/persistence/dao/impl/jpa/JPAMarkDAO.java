c9b3d7c3be7ea598811dd8037db46fd36732123f
Bohdan Khudoba <bohdanthebest@gmail.com>
2015-09-29 09:07:55 +0300

    add annotation for JPAMarkDAO

--- persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAMarkDAO.java
+++ persistence/src/main/java/com/softserveinc/ita/jresume/persistence/dao/impl/jpa/JPAMarkDAO.java
@@ -1,11 +1,14 @@
 package com.softserveinc.ita.jresume.persistence.dao.impl.jpa;
 
+import org.springframework.stereotype.Repository;
+
 import com.softserveinc.ita.jresume.common.entity.Mark;
 import com.softserveinc.ita.jresume.persistence.dao.impl.MarkDAO;
 /**
  * JPA implementation for MarkDAO.
  * @author Golovii Sergii
  */
+@Repository
 public class JPAMarkDAO extends JPAGenericDAO<Mark, Long> implements MarkDAO {
 
 }
