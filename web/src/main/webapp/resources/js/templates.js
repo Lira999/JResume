01f94a0be8eb6ba503e8df5723180805dfb0a51c
Ira Lialikova <lialikova.ira@gmail.com>
2015-10-05 15:35:42 +0300

    Add error message to display on templates page, add demo template images.

--- web/src/main/webapp/resources/js/templates.js
+++ web/src/main/webapp/resources/js/templates.js
@@ -1,36 +1,45 @@
 $(document).ready(function() {
- sendAjax();
+    sendAjax();
 }); 
+
 function sendAjax() {
-  $.ajax({
-  url: "templates/list",
-  type: "GET",
-  contentType: "application/json",
-  success: function(data) {
-   $.each(data ,function(index, templates) {
+    $.ajax({
+    url: "templates/list",
+    type: "GET",
+    contentType: "application/json",
+    success: function(data) {
+    $.each(data ,function(index, templates) {
 	   
-	   var address = new String("viewtempl" + templates.id);
-	   if (templates.price == null) { var priceButton = new String("<a href=" + address + " class='btn button btn-success'>Free</a>"); }
-	   		else { var priceButton = new String("<a href=" + address + " class='btn button btn-danger'>" + "Price " + templates.price + "$" +"</a>") }
-	  
-	   $("<div>" +
-	   		"<div class='thumbnail'>" +
+	    var address = new String("viewtempl" + templates.id);
+	    if (templates.price == null) { var priceButton = new String("<a href=" + address + " class='btn button btn-success'>Free</a>"); }
+	   	    else { var priceButton = new String("<a href=" + address + " class='btn button btn-danger'>" + "Price " + templates.price + "$" +"</a>") }
+	    var image = new String("<img src='./resources/templates/" + templates.name + ".png'>");
+	   
+	    $("<div>" +
+	   	    "<div class='thumbnail'>" +
 	   		"<div class='caption'>" +
-	   		priceButton +
+	   		    priceButton +
 	   		"</div>" +
-	   		"<img src='./resources/images/Template1.png'>" + 
+	   		    image + 
 	   		"<p class='template-name'>" + templates.name + "</p>" +
 	   		"</div>" +
 	   		"</div>")
-		   .addClass("col-sm-12 col-xs-12 col-md-4 col-lg-4")
-		   .attr('id', templates.id)
-		   .appendTo("div#row");  
-		 	   
-   });   
-     
-  },
-  error:function(data,status,er) {
-	  
-	    }
- });
+		        .addClass('col-sm-12 col-xs-12 col-md-4 col-lg-4')
+		        .attr('id', templates.id)
+		        .appendTo('div#row');  		 	   
+    });        
+    },
+    error:function(data,status,er) {
+	$("<div>" +
+	    "<p><strong>Error:</strong>" + 
+			"JResume encountered some internal problems. " +
+            "We working to resolve the issue as " +
+            "soon as possible." + 
+            " Please, try again later." + 
+		"</p>" +
+	  	"</div>")
+	  	    .addClass('error alert alert-danger fade in')	   
+	  	    .appendTo('div#row');  	  
+	}
+    });
 }
