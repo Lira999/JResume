b2fc8f379af03c3ae43216ca115cffe6f3c45ce0
Ira Lialikova <lialikova.ira@gmail.com>
2015-11-06 01:59:19 +0200

    optimize code in templates.js

--- web/src/main/webapp/resources/js/templates.js
+++ web/src/main/webapp/resources/js/templates.js
@@ -1,13 +1,10 @@
-var DEFAULT_PAGE = 1;
-var PAGE_SIZE = 6;
-	
 $(document).ready(function() {
+	var DEFAULT_PAGE = 1;
+	var PAGE_SIZE = 6;
 	sendAjax();
 	$('#select').change(function() {
 		$('#select option').each(sendAjax());
 	});	
-});
-
 function sendAjax() {
 	$.ajax({
 		url : 'templates/list?viewOnly=' + $('#view').val() + '&sortBy='
@@ -15,23 +12,17 @@ function sendAjax() {
 		type : 'GET',
 		contentType : 'application/json',
 		success : function(templates) {
-			$('#paging').empty();
-			var dataLength = templates.length;
-			var numPage = 1;
-			while(dataLength > 0) {
+			var maxPage = Math.ceil(templates.length/PAGE_SIZE);
+			showTemplate(DEFAULT_PAGE);
+			for(var i = 1; i <= maxPage; i++) {
 				$('<input type="button" class="btn btn-default" name="' 
-						+ numPage + '" value="' + numPage + '">')
-						.appendTo('#paging')
-				dataLength -= PAGE_SIZE;
-				numPage++;
-			}		
-			
+						+ i + '" value="' + i + '">')
+						.appendTo('#paging');
+			}			
 			function showTemplate(page) {
-				var currentPage = page;
-				var startPosition = (currentPage - 1)*PAGE_SIZE;
-				var endPosition = (currentPage - 1)*PAGE_SIZE + PAGE_SIZE;
 				$('#row').empty();
-				for(var i = startPosition; i < endPosition; i++ ) {
+				for(var i = (page - 1)*PAGE_SIZE; 
+				        i < (page - 1)*PAGE_SIZE + PAGE_SIZE; i++ ) {
 					var address = new String('viewtemplate/' + templates[i].id);
 					if (templates[i].price == null) {
 						var priceButton = new String('<a href=' + address
@@ -41,29 +32,25 @@ function sendAjax() {
 								+ ' class="btn button btn-danger">' + 'Price '
 								+ templates[i].price + '$' + '</a>')
 					}
-					var image = new String('<img src="/Files/' + templates[i].name 
-							+ '.png">');
-					$(
-							'<div>' + '<div class="thumbnail">'
-									+ '<div class="caption">' + priceButton
-									+ '</div>' + image
-									+ '<p class="template-name">' + templates[i].name
-									+ '</p>' + '</div>' + '</div>')
-							.addClass('col-sm-12 col-xs-12 col-md-4 col-lg-4')
-							.attr('id',	templates[i].id)
-							.appendTo('div#row');
+					var image = new String('<img src="/Files/' 
+							+ templates[i].name + '.png">');
+					$('<div>' + '<div class="thumbnail">'
+						      + '<div class="caption">' + priceButton
+							  + '</div>' + image
+							  + '<p class="template-name">' + templates[i].name
+							  + '</p>' + '</div>' + '</div>')
+							  .addClass('col-sm-12 col-xs-12 col-md-4 col-lg-4')
+							  .attr('id',	templates[i].id)
+							  .appendTo('div#row');
 				}
 			}
-
-			showTemplate(DEFAULT_PAGE);
-			
-			$('#paging :button').click(function(){
+    		$('#paging :button').click(function(){
 				showTemplate(this.value);
-			});
-			
+			});			
 		},
 		error : function(templates, status, er) {
 			showErrorMessage('div#row');
 		}
 	});
 }
+});
